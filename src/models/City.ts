0cdae2a5ab8e5bae3a8e9d36397e60c66a7bed53
OttaviaFeletig <ottafel@hotmail.it>
2019-10-31 16:08:31 +0100

    city schema mongoose

---
+++ src/models/City.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export default interface CitySchemaData extends Document {
+  name: string;
+  country: string;
+  picture: string;
+}
+const City: Schema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  country: {
+    type: String,
+    required: true
+  },
+  picture: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model<CitySchemaData>("city", City);
