dbc985fa29029995719da473af03c3d402e7671f
OttaviaFeletig <ottafel@hotmail.it>
2019-10-31 16:42:08 +0100

    get city route

--- src/routes/api/city.ts
+++ src/routes/api/city.ts
@@ -2,28 +2,37 @@ import express = require("express");
 const router: express.Router = express.Router();
 const City = require("../../models/City");
 import { CityI } from "../../types/City";
-import { Schema } from "mongoose";
 import CitySchemaData from "../../models/City";
 router.get("/test", (req, res) => res.send("Testing city route"));
 router.post("/", async (req, res) => {
   const { name, country, picture }: CityI = req.body;
-  const cityDB: CityI = await City.findOne({ name: name, country: country });
-  console.log("cityDB", cityDB);
-  if (cityDB === null) {
-    const newCity: CitySchemaData = new City({
-      name: name,
-      country: country,
-      picture: picture
-    });
-    try {
-      const savedCity = await newCity.save();
-      console.log("savedCity", savedCity);
-      res.json(savedCity);
-    } catch (error) {
-      console.log("error", error);
+  try {
+    const cityDB: CityI = await City.findOne({ name: name, country: country });
+    if (cityDB) {
+      res.status(404).send("City Already In DB");
+    } else {
+      const newCity: CitySchemaData = new City({
+        name: name,
+        country: country,
+        picture: picture
+      });
+      try {
+        const savedCity = await newCity.save();
+        res.send(savedCity);
+      } catch (error) {
+        res.send(error);
+      }
     }
-  } else {
-    res.json({ error: "This city already exists in the DB" });
+  } catch (error) {
+    res.send(error);
+  }
+});
+router.get("/", async (req, res) => {
+  try {
+    const cities: Array<CityI> = await City.find({});
+    res.send(cities);
+  } catch (error) {
+    res.send(error);
   }
 });
 module.exports = router;
